c5c96e7c72cba29584f8a415a7c20fb20b46573a
inhyuck222 <inhyuck222@gmail.com>
2018-03-16 17:49:08 +0900

    insert repost

--- src/com/cafe24/mysite/action/board/BoardActionFactory.java
+++ src/com/cafe24/mysite/action/board/BoardActionFactory.java
@@ -12,7 +12,13 @@ public class BoardActionFactory extends AbstractActionFactory {
 		if(actionName == null) {
 			actionName = "";
 		}
-		
+		/*
+		 * g_no(2) = 유지 => 2
+		 * o_no(1) = o_no(1) + 1 => 2
+		 * d(0) = d + 1 => 1
+		 * update o_no = o_no+1 where g_no = 2 and o_no >= 2
+		 * insert (~~~~)
+		 */
 		switch(actionName) {
 		case "newpostwrite" : 
 			action = new NewPostWriteAction();
